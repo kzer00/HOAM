77c2a5f940184eb75d826323ffd44d6b497fd862
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-02-05 14:52:27 +0700

    Update imagebuilder.sh

--- router-config/openwrt-imagebuilder/imagebuilder.sh
+++ router-config/openwrt-imagebuilder/imagebuilder.sh
@@ -76,14 +76,6 @@ download_imagebuilder() {
 # Adjust related files in the ImageBuilder directory
 adjust_settings() {
     cd ${imagebuilder_path}
-    mkdir -p etc/uci-defaults
-    cd ${imagebuilder_path}
-    mkdir -p usr/share/rpcd/acl.d
-    cd ${imagebuilder_path}
-    wget -P etc/uci-defaults https://raw.githubusercontent.com/kzer00/repo/main/aarch64_cortex-a53/30_interfaces.sh
-    #wget -P /etc/uci-defaults https://raw.githubusercontent.com/kzer00/repo/main/aarch64_cortex-a53/90_wifi.sh
-    wget -P www/luci-static/resources/view/status/include https://raw.githubusercontent.com/kzer00/repo/main/aarch64_cortex-a53/29_port.js
-    wget -q https://raw.githubusercontent.com/kzer00/repo/main/aarch64_cortex-a53/luci-mod-status-index.json -O usr/share/rpcd/acl.d/luci-mod-status-index.json
     echo "src/gz custom_repo https://raw.githubusercontent.com/kzer00/repo/main/aarch64_cortex-a53" >> repositories.conf
     sed -i 's/option check_signature/# option check_signature/g' repositories.conf
     echo -e "${STEPS} Start adjusting .config file settings..."
